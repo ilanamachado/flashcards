99f4562f5d2fb228cf7c80601e07f6af2199b6ab
ilanamachado <164919130+ilanamachado@users.noreply.github.com>
2024-11-25 14:17:06 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,15 +1,35 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-   ' O Python é uma linguagem de programação.'
+    'O Python é uma linguagem de programação.'
 )
 
 criaCartao(
-'Geografia',
-'Qual a capital da França?',
-'A capital da frança é em Paris'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
 )
 
 criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
 
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI.'
+)
+
+criaCartao(
+    'matematica',
+    'Quanto é 30 dividido por 19?',
+    '30 dividido por 19 é 1,57.'
+)
+
+riaCartao(
+    'Lingua Portuguesa',
+    'O que é hipérbole?',
+    'Hipérbole é uma exageração usada para enfatizar uma idéia.'
 )
